21354dab162ace82942028f20a33162365281bc4
iulia <iulia.heinrich@gmail.com>
2020-09-28 10:57:41 -0700

    Set up Google Analytics

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactGA from 'react-ga';
 import NavBarContainer from "./nav_bar/nav_bar_container";
 import HomePageContainer from "./home_page/home_page_container";
 import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
@@ -9,6 +10,11 @@ import PhotoContainer from "./photo/photo_container";
 import PhotoFormContainer from "./photo/photo_form";
 import TagFormContainer from "./tag/tag_form_container";
 import PhotosContainer from "./photo/photos";
+
+function initializeReactGA() {
+    ReactGA.initialize('UA-179152947-1');
+    ReactGA.pageview('/homepage');
+}
 
 
 const App = () => (
